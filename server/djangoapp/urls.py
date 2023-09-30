62a74ea6c9c9140448779c85dd37bf5e90376e52
alasow <calasow029@hotmail.com>
2023-09-30 09:31:09 +0300

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,24 +10,30 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-    path(route = 'about/', view = views.about, name='about'),
+     path(route='about', view=views.about, name='about'),
 
+   
     # path for contact us view
-    path(route = 'contact/', view = views.contact, name='contact'),
-
+     path(route='contact', view=views.contact, name='contact'),
     # path for registration
-    path('signup/', views.registration_request, name='registration'),
-    # path for login
-    path('login/', views.login_request, name='login'),
-    # path for logout
-    path('logout/', views.logout_request, name='logout'),
+    path(route='registration', view=views.registration_request, name='registration'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+    # path for login
+    path(route='login', view=views.login_request, name='login'),
+    # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+
+    
 
     # path for dealer reviews view
-    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
+    path(route='', view=views.get_dealerships, name='index'),
+
+     path('dealer/<int:dealer_id>/',
+         view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
-    path('dealer/<int:dealer_id>/add_review/', views.add_review, name='add_review')
+    #path('review/<int:dealer_id>/',
+    #     view=views.add_review, name='add_review'),
+     path(route='dealer/<int:dealer_id>/add-review/', view=views.add_review, name="add_review")
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
