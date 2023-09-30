d103ea193555e18905c0062e30e224f24890259d
alasow <calasow029@hotmail.com>
2023-09-30 09:29:56 +0300

    Create get-reviews.js

---
+++ functions/sample/get-reviews.js
@@ -0,0 +1,41 @@
+function main(params) {
+    
+    return new Promise(function (resolve, reject) {
+      const { CloudantV1 } = require("@ibm-cloud/cloudant");
+      const { IamAuthenticator } = require("ibm-cloud-sdk-core");
+      const authenticator = new IamAuthenticator({
+        apikey: "nBCEM7dnCfeiu_EVIqpQm86GX2gK01ppgK-Nq52A59o-", // TODO: Replace with your own API key
+      });
+      const cloudant = CloudantV1.newInstance({
+        authenticator: authenticator,
+      });
+      cloudant.setServiceUrl("https://c17f6666-d70a-4bf8-b878-4fd067afaea5-bluemix.cloudantnosqldb.appdomain.cloud"); // TODO: Replace with your own service URL
+      dealership = 7;
+      // return reviews with this dealer id
+      cloudant
+        .postFind({
+          db: "reviews",
+          selector: {
+            dealership: 29,
+          },
+        })
+        .then((result) => {
+          let code = 200;
+          if (result.result.docs.length == 0) {
+            code = 404;
+          }
+          resolve({
+            statusCode: code,
+            headers: { "Content-Type": "application/json" },
+            body: result.result.docs,
+          });
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+  
+  // example invocation
+  let result = main({ dealerId: 29 });
+  result.then((reviews) => console.log(reviews));
